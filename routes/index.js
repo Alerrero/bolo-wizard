644629f3360999b3bb37506b0da321a9040e482d
Alejandro Herrero <a.herrerolorente@gmail.com>
2021-02-12 14:01:12 +0100

    Ticketmaster API install and routes set

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,11 @@ module.exports = app => {
 
     // Base URLS
     app.use('/', require('./base.routes.js'))
+    app.use('/events', require('./events.routes.js'))
+    app.use('/api', require('./api.routes.js'))
+    app.use('/auth', require('./auth.routes.js'))
+
+
+    app.use('/places', require('./places.routes.js'))
+
 }
