52935e038f2d2830b739315c9d362ef26ba69975
Alejandro Herrero <a.herrerolorente@gmail.com>
2021-02-15 15:06:16 +0100

    Spotify

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
-const ticketmasterVenues = 'https://app.ticketmaster.com/discovery/v2/venues.json?countryCode=ES&apikey=HdYZa33NpSQm7RcffP412uoFrL5obLRL'
-
+const ticketmasterVenues = `https://app.ticketmaster.com/discovery/v2/venues.json?countryCode=ES&apikey=${process.env.TMKEY}`
+//TODO Key al env
 const removeDups = (someArr) => someArr.filter((v,i) => someArr.indexOf(v) === i)
 const normalizeText = (someStrg) => someStrg.normalize('NFD').replace(/[\u0300-\u036f]/g,"")
 
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 
     axios.get(ticketmasterVenues)
         .then(response => {
-            
+            //TODO APROVECHAR MAP
             const venues = response.data._embedded.venues
             const cities = []
             const normalizedCities = []
